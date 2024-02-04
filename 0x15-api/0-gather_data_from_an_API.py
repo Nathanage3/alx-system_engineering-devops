c77f855449e3801d6f785f0aeb50ea65d7830fb7
Nathanage3 <sisay954@gmail.com>
2024-02-04 16:26:16 +0300

    fetch data and display

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """Returns to-do list information for a given employee ID."""
+
 import requests
 import sys
+
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
