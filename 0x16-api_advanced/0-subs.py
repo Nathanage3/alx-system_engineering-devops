8e772b90fb8c1905676c72a7c7c279941a080d8e
Nathanage3 <sisay954@gmail.com>
2024-02-14 14:42:23 +0300

    Ask reddit API how many subscribers in subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,34 @@
 #!/usr/bin/python3
-"""queries the Reddit API and returns the number of subscribers
-(not active users, total subscribers) for a given subreddit
-"""
 import requests
 
-
 def number_of_subscribers(subreddit):
+    """
+    Queries the Reddit API and returns the number of subscribers for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: Number of subscribers (total subscribers) for the subreddit. Returns 0 if invalid subreddit.
+    """
+    # Set a custom User-Agent to avoid Too Many Requests errors
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) Apple' +
         'WebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
     }
-    url = 'https://www.reddit.com/r/{:}/about.json'.format(
-        subreddit)
-    r = requests.get(url, headers=headers, allow_redirects=False)
-    if r.status_code >= 300:
-        return 0
-    json = r.json()
-    data_dict = json.get('data')
-    return(data_dict.get('subscribers'))
+
+    # Construct the API URL for the subreddit
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+
+    try:
+        # Make the API request
+        response = requests.get(url, headers=headers)
+        data = response.json()
+
+        # Check if the subreddit exists
+        if 'data' in data and 'subscribers' in data['data']:
+            return data['data']['subscribers']
+        else:
+            return 0  # Invalid subreddit
+    except requests.RequestException:
+        return 0  # Error occurred or invalid subreddit
