2955d4f6aed8e80aba5d02ea3d3323cd684c85f9
Nathanage3 <sisay954@gmail.com>
2024-02-14 22:04:43 +0300

    task #1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+'''#!/usr/bin/python3
 """
 Queries the Reddit API and returns the number of subscribers
 (not active users, total subscribers) for a given subreddit.
@@ -18,5 +18,20 @@ def number_of_subscribers(subreddit):
     json_data = r.json()
     data_dict = json_data.get('data')
     return data_dict.get('subscribers')
+    '''
+#!/usr/bin/python3
+
+import requests as r
 
 
+def number_of_subscribers(subreddit):
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:73.0) \
+        Gecko/20100101 Firefox/73.0"
+    }
+    response = r.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
