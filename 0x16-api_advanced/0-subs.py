493ac6238789af7db0592794eecbe1e773debc9b
Nathanage3 <sisay954@gmail.com>
2024-02-14 16:21:37 +0300

    Ask reddit API how many subscribers in subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,8 @@ import requests
 
 def number_of_subscribers(subreddit):
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) Apple' +
+        'WebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
     }
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     r = requests.get(url, headers=headers, allow_redirects=False)
