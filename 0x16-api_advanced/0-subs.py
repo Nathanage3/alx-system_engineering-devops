8fb81cff42b6b37498d86b286865ce94869217b1
Nathanage3 <sisay954@gmail.com>
2024-02-15 21:39:31 +0300

    edited task #0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""Quaries for requesting reddit the number of subscriber"""
 import requests as r
 
 
 def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "CustomUserAgent"
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:73.0) \
+        Gecko/20100101 Firefox/73.0"
     }
     response = r.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
