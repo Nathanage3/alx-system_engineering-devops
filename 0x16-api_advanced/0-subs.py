e9b72f09e4c6fbc29ab42694d7650b1cc5c4f098
Nathanage3 <sisay954@gmail.com>
2024-02-15 21:34:42 +0300

    edited task #0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,34 +1,12 @@
-'''#!/usr/bin/python3
-"""
-Queries the Reddit API and returns the number of subscribers
-(not active users, total subscribers) for a given subreddit.
-"""
-
-import requests
-
-def number_of_subscribers(subreddit):
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) Apple' +
-        'WebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
-    }
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    r = requests.get(url, headers=headers, allow_redirects=False)
-    if r.status_code >= 300:
-        return 0   
-    json_data = r.json()
-    data_dict = json_data.get('data')
-    return data_dict.get('subscribers')
-    '''
 #!/usr/bin/python3
-
+"""Quaries for requesting reddit the number of subscriber"""
 import requests as r
 
 
 def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:73.0) \
-        Gecko/20100101 Firefox/73.0"
+        "User-Agent": "CustomUserAgent"
     }
     response = r.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,22 +1,29 @@
 #!/usr/bin/python3
-"""queries the Reddit API and prints the titles of the first 10 hot posts
+"""
+Queries the Reddit API and prints the titles of the first 10 hot posts
 listed for a given subreddit.
 """
-import requests
 
+import requests
+import sys
 
 def top_ten(subreddit):
     headers = {
-        'User-Agent': 'Custom User Agent'
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) Apple' +
+        'WebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
     }
-    r = requests.get('https://www.reddit.com/r/{:}/hot.json?limit=10'.format(
-        subreddit), headers=headers, allow_redirects=False)
-    if r.status_code < 300:
-        json = r.json()
-        data_dict = json.get('data')
-        hot_list = data_dict.get('children')
-        for post in hot_list:
-            sub_data_dict = post.get('data')
-            print(sub_data_dict.get('title'))
-    else:
-        print(None)
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
+    
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        response.raise_for_status()  # Raise an exception if status code >= 400
+        data = response.json()
+        posts = data.get('data', {}).get('children', [])
+        
+        for post in posts:
+            title = post.get('data', {}).get('title', '')
+            print(title)
+            if not posts:
+                print("None")
+    except requests.RequestException as e:
+        print(f"Error: {e}")
